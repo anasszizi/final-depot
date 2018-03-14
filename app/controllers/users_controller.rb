083be42bd45ba2ef9c52f58a1b36fef6eb8b916b
anasszizi <anasszizi@gmail.com>
2018-03-14 11:51:24 -0400

    final depot

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,28 +26,28 @@ def index
   def create
 @user = User.new(user_params)
 respond_to do |format|
-  if @user.save
-=>  format.html { redirect_to users_url, notice: "User #{@user.name} was successfully created." }
-    format.json { render :show, status: :created, location: @user }
-  else
-    format.html { render :new }
-    format.json { render json: @user.errors, status: :unprocessable_entity }
-  end
- end
+      if @user.save
+        format.html { redirect_to users_url, notice: "User #{@user.name} was successfully created." }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
- respond_to do |format|
-  if @user.update(user_params)
-=>  format.html { redirect_to users_url, notice: "User #{@user.name} was successfully updated." }
-    format.json { render :show, status: :ok, location: @user }
-  else
-    format.html { render :edit }
-    format.json { render json: @user.errors, status: :unprocessable_entity }
-  end
- end
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to users_url, notice: "User #{@user.name} was successfully updated." }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   # DELETE /users/1
